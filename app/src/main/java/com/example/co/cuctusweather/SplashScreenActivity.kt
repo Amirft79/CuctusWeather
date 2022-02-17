19a70b209bb12afa5479d066c0b423c2c05a9b4c
amirhossein fardi <amithosseine2013@gmail.com>
2022-02-17 13:55:13 +0330

    second commit

---
+++ app/src/main/java/com/example/co/cuctusweather/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.co.cuctusweather
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.co.cuctusweather.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.image.alpha=0f
+        binding.image.animate().setDuration(2000).alpha(1f).withEndAction{
+            val intent=Intent(this,StartActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+}
--- app/src/main/java/com/example/co/cuctusweather/StartActivity.kt
+++ app/src/main/java/com/example/co/cuctusweather/StartActivity.kt
@@ -1,15 +1,110 @@
 package com.example.co.cuctusweather
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import android.os.Bundle
+import android.os.CancellationSignal
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.co.cuctusweather.databinding.ActivityStartBinding
+import java.util.*
+import kotlin.concurrent.schedule
 
 class StartActivity : AppCompatActivity() {
+    private var cancelationsignal:CancellationSignal?=null
+    private val autenticationcallback:BiometricPrompt.AuthenticationCallback
+        get() =
+            @RequiresApi(Build.VERSION_CODES.P)
+            object :BiometricPrompt.AuthenticationCallback(){
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                    super.onAuthenticationError(errorCode, errString)
+                    ShowToast("error $errString")
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                    super.onAuthenticationSucceeded(result)
+                    val intent= Intent(this@StartActivity,HomeActivity::class.java)
+                    startActivity(intent)
+                    ShowToast("welcome !!!!!")
+                }
+
+            }
     private lateinit var  binding:ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityStartBinding.inflate(layoutInflater)
+        binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        CheckedBiometricsupport()
+        val biometricPrompt: BiometricPrompt = BiometricPrompt.Builder(this)
+            .setTitle("fingetprint")
+            .setTitle("your security is needed")
+            .setDescription("this app is use the finger print")
+            .setNegativeButton(
+                "cancel",
+                this.mainExecutor,
+                DialogInterface.OnClickListener { dialogInterface, i ->
+                    ShowToast("canel")
+                }).build()
+        biometricPrompt.authenticate(getcancelationsignal(),mainExecutor,autenticationcallback)
+        //btn_on_finger
+        binding.btnUserFinger.setOnClickListener {
+            val biometricPrompt: BiometricPrompt = BiometricPrompt.Builder(this)
+                .setTitle("fingetprint")
+                .setTitle("your security is needed")
+                .setDescription("this app is use the finger print")
+                .setNegativeButton(
+                    "cancel",
+                    this.mainExecutor,
+                    DialogInterface.OnClickListener { dialogInterface, i ->
+                        ShowToast("canel")
+                    }).build()
+            biometricPrompt.authenticate(getcancelationsignal(),mainExecutor,autenticationcallback)
+        }
+
+    }
+    //Support_finger
+    private fun CheckedBiometricsupport() : Boolean {
+        val keygardmanager: KeyguardManager =getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        if (!keygardmanager.isDeviceSecure){
+            ShowToast("your device dose not have finger print")
+            return false
+        }
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.USE_BIOMETRIC)
+            != PackageManager.PERMISSION_GRANTED){
+            ShowToast("finger print is not enable")
+            return false
+        }
+        return if(packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)){
+            true
+        } else true
+    }
+    private fun getcancelationsignal(): CancellationSignal {
+        cancelationsignal= CancellationSignal()
+        cancelationsignal?.setOnCancelListener {
+            ShowToast("your fingerprint is canceld by user")
+        }
+        return cancelationsignal as CancellationSignal
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val biometricPrompt: BiometricPrompt = BiometricPrompt.Builder(this)
+            .setTitle("fingetprint")
+            .setTitle("your security is needed")
+            .setDescription("this app is use the finger print")
+            .setNegativeButton(
+                "cancel",
+                this.mainExecutor,
+                DialogInterface.OnClickListener { dialogInterface, i ->
+                    ShowToast("canel")
+                }).build()
+        biometricPrompt.authenticate(getcancelationsignal(),mainExecutor,autenticationcallback)
 
     }
 }
